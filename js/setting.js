9aaa0abe8d0f3e0da99f4187c0e1212776603186
ishihara_takashi <ishihara_note02@ishihara-note02.local>
2014-06-09 23:03:29 +0900

    Fix icon setting

--- js/setting.js
+++ js/setting.js
@@ -17,31 +17,136 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 
 OpenSpending.Styles.Cofog = {
 
-  /* yokohama cofog */
-  '1': { icon: 'icons/helping-others.svg', color: '#20749A', bcolor: '#20749A' },
-  'project-1': { icon: 'icons/helping-others.svg', color: '#20749A', bcolor: '#20749A' },
-  '2': { icon: 'icons/schools.svg', color: '#20749A', bcolor: '#20749A' },
-  'project-2': { icon: 'icons/schools.svg', color: '#20749A', bcolor: '#20749A' },
-  '3': { icon: 'icons/economy-tourism.svg', color: '#20749A', bcolor: '#20749A' },
-  'project-3': { icon: 'icons/economy-tourism.svg', color: '#20749A', bcolor: '#20749A' },
-  '4': { icon: 'icons/our-streets.svg', color: '#20749A', bcolor: '#20749A' },
-  'project-4': { icon: 'icons/our-streets.svg', color: '#20749A', bcolor: '#20749A' },
-  '5': { icon: 'icons/environment.svg', color: '#20749A', bcolor: '#20749A' },
-  'project-5': { icon: 'icons/environment.svg', color: '#20749A', bcolor: '#20749A' },
-  '6': { icon: 'icons/civilian-action.svg', color: '#20749A', bcolor: '#20749A' },
-  'project-6': { icon: 'icons/civilian-action.svg', color: '#20749A', bcolor: '#20749A' },
-  '7': { icon: 'icons/traffic-watersup.svg', color: '#20749A', bcolor: '#20749A' },
-  'project-7': { icon: 'icons/traffic-watersup.svg', color: '#20749A', bcolor: '#20749A' },
-  '8': { icon: 'icons/government.svg', color: '#20749A', bcolor: '#20749A' },
-  'project-8': { icon: 'icons/government.svg', color: '#20749A', bcolor: '#20749A' },
-  '9': { icon: 'icons/garbage.svg', color: '#20749A', bcolor: '#20749A' },
-  'project-9': { icon: 'icons/garbage.svg', color: '#20749A', bcolor: '#20749A' },
-  '10': { icon: 'icons/order-safety.svg', color: '#20749A', bcolor: '#20749A' },
-  'project-10': { icon: 'icons/order-safety.svg', color: '#20749A', bcolor: '#20749A' },
-  '11': { icon: 'icons/order-safety.svg', color: '#20749A', bcolor: '#20749A' },
-  'project-11': { icon: 'icons/order-safety.svg', color: '#20749A', bcolor: '#20749A' },
-  '12': { icon: 'icons/order-safety.svg', color: '#20749A', bcolor: '#20749A' },
-  'project-12': { icon: 'icons/order-safety.svg', color: '#20749A', bcolor: '#20749A' },
+	// 議会費
+	'1': {
+		icon: 'icons/economic-aid.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	'project-1': {
+		icon: 'icons/economic-aid.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	// 総務費
+	'2': {
+		icon: 'icons/publicaffairs.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	'project-2': {
+		icon: 'icons/publicaffairs.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	// 民生費
+	'3': {
+		icon: 'icons/family.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	'project-3': {
+		icon: 'icons/family.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	// 衛生費
+	'4': {
+		icon: 'icons/health.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	'project-4': {
+		icon: 'icons/health.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	// 衛生費
+	'5': {
+		icon: 'icons/environment.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	'project-5': {
+		icon: 'icons/environment.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	// 商工費
+	'6': {
+		icon: 'icons/economy-tourism.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	'project-6': {
+		icon: 'icons/economy-tourism.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	// 土木費
+	'7': {
+		icon: 'icons/coal.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	'project-7': {
+		icon: 'icons/coal.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	// 消防費
+	'8': {
+		icon: 'icons/fire-brigade.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	'project-8': {
+		icon: 'icons/fire-brigade.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	// 教育費
+	'9': {
+		icon: 'icons/schools.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	'project-9': {
+		icon: 'icons/schools.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	// 災害復旧日
+	'10': {
+		icon: 'icons/aid-developing-countries.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	'project-10': {
+		icon: 'icons/aid-developing-countries.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	// 公債費
+	'11': {
+		icon: 'icons/money.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	'project-11': {
+		icon: 'icons/money.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	// 予備費
+	'12': {
+		icon: 'icons/pig.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
+	'project-12': {
+		icon: 'icons/pig.svg',
+		color: '#20749A',
+		bcolor: '#20749A'
+	},
 };
-
-
